c384c0cf743c408d22d38b601fff9de9767689be
Jeferson Melo <jeferosnmedeiros000@gmail.com>
2023-02-15 21:55:38 -0300

    Adds Mateiral Design Components library
    Signed-off-by: Jeferson Melo <jeferosnmedeiros000@gmail.com>

--- brand-generator-frontend/src/App.jsx
+++ brand-generator-frontend/src/App.jsx
@@ -1,11 +1,12 @@
-
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>        
+        <h1>Hello world</h1>   
       </header>
+        <Button variant="contained">Hello World</Button>
     </div>
   );
 }
--- brand-generator-frontend/src/index.js
+++ brand-generator-frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
