9d809bba6438f51669e93aba1e0dfee4244151c1
Jeferson Melo <jeferosnmedeiros000@gmail.com>
2023-02-23 20:58:17 -0300

    Improves the gpt3.py service File minimizing the number of OpenAI api calls
    Signed-off-by: Jeferson Melo <jeferosnmedeiros000@gmail.com>

--- brand-generator-frontend/src/models/business.js
+++ brand-generator-frontend/src/models/business.js
@@ -1,3 +1,3 @@
-const business = { name: null, keywords: ["Technology"],type:"Shop",description:null,atributes: ["Creative"]};
+const business = { name: "", keywords: ["Technology"],type:"Shop",description:"",atributes: ["Creative"]};
 
 export default business
--- brand_generator_backend/brand_aspects/services/gpt3.py
+++ brand_generator_backend/brand_aspects/services/gpt3.py
@@ -5,25 +5,41 @@ import json
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 def generate_brand(business_info):
-    try:
-        if business_info["description"] !="":
-            description=business_info["description"]
-        else:
-            description=_generate_brand_description(business_info)["description"]
-    except:
-        description=_generate_brand_description(business_info)["description"]
-    if business_info["name"] !="":
-        name=business_info["name"]
-        acronym=_generate_brand_acronym(business_info)["acronym"]
-    else:
-        generated = _generate_brand_name(business_info)
-        name = generated["name"]
-        acronym=generated["acronym"]
-    
-    slogan =_generate_brand_slogan(business_info,name)["slogan"]
-    color_scheme=_generate_brand_colors(business_info)["colors"]
-    font = _generate_brand_font(business_info)["font"]
-    return {"name":name,"slogan":slogan,"achronym":acronym,"color_scheme":color_scheme, "description":description,"font":font}
+    if business_info["description"] =="" and business_info["name"]=="":
+        brand= _generate_brand_name_and_description(business_info)
+        font_and_colors = _generate_brand_font_and_colors(business_info)
+        return {"name":brand['name'],"slogan":brand['slogan'],"achronym":brand['acronym'],"color_scheme":font_and_colors['colors'], "description":brand['description'],"font":font_and_colors['font']}
+
+    if business_info["description"] !="" and business_info["name"]=="":
+        brand= _generate_brand_name(business_info)
+        font_and_colors = _generate_brand_font_and_colors(business_info)
+        return {"name":brand['name'],"slogan":brand['slogan'],"achronym":brand['acronym'],"color_scheme":font_and_colors['colors'], "description":business_info["description"],"font":font_and_colors['font']}
+
+    if business_info["description"] =="" and business_info["name"]!="":
+        brand= _generate_brand_description(business_info)
+        font_and_colors = _generate_brand_font_and_colors(business_info)
+        return {"name":business_info['name'],"slogan":brand['slogan'],"achronym":brand['acronym'],"color_scheme":font_and_colors['colors'], "description":brand["description"],"font":font_and_colors['font']}
+
+def _generate_brand_name_and_description(business_info):
+    def _generate_brand_name_prompt(business_info):
+        def _separate_by_comas(list):
+            separeted_string=""
+            for word in list:
+                separeted_string+=word+","
+            return separeted_string
+
+        business_type=business_info["type"]
+        keywords = _separate_by_comas(business_info["keywords"])
+        atributes = _separate_by_comas(business_info["atributes"])
+        return f"""Sugest a name, an acronym, a slogan and a description with 20 to 30 words for a business that is a {business_type} related to  {keywords}.They must be  {atributes}. Return as a JSON string. Ex:{{"name":"Busines Generator","slogan":"Get new gen with business generator","description":""}}"""
+    response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=_generate_brand_name_prompt(business_info),
+            temperature=0.7,
+            presence_penalty=1.5,
+            max_tokens=400
+        )
+    return json.loads(response["choices"][0]["text"])
 
 def _generate_brand_name(business_info):
     def _generate_brand_name_prompt(business_info):
@@ -36,21 +52,7 @@ def _generate_brand_name(business_info):
         business_type=business_info["type"]
         keywords = _separate_by_comas(business_info["keywords"])
         atributes = _separate_by_comas(business_info["atributes"])
-        return f"""Sugest a name and an acronym for a business that is a {business_type} related to  {keywords}.The name must be  {atributes}. Return as a JSON string with name and acronym keys.Example:{{"name":"name"}}"""
-    response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=_generate_brand_name_prompt(business_info),
-            temperature=0.7,
-            presence_penalty=1.5,
-            max_tokens=400
-        )
-    return json.loads(response["choices"][0]["text"])
-
-def _generate_brand_acronym(business_info):
-    def _generate_brand_name_prompt(business_info):
-
-        name=business_info['name']
-        return f"""Sugest an acronym for a business that is a called {name}. Return as a JSON String.Example:{{"acronym":"LTS"}}"""
+        return f"""Sugest a name, an acronym and a slogan for a business that is a {business_type} related to  {keywords}.They must be  {atributes}. Return as a JSON string. Ex:{{"name":"Busines Generator","slogan":"Get new gen with business generator","acronym":"BG"}}"""
     response = openai.Completion.create(
             model="text-davinci-003",
             prompt=_generate_brand_name_prompt(business_info),
@@ -61,7 +63,7 @@ def _generate_brand_acronym(business_info):
     return json.loads(response["choices"][0]["text"])
 
 def _generate_brand_description(business_info):
-    def _generate_brand_description_prompt(business_info):
+    def _generate_brand_name_prompt(business_info):
         def _separate_by_comas(list):
             separeted_string=""
             for word in list:
@@ -69,60 +71,22 @@ def _generate_brand_description(business_info):
             return separeted_string
 
         business_type=business_info["type"]
+        name = business_info['name']
         keywords = _separate_by_comas(business_info["keywords"])
         atributes = _separate_by_comas(business_info["atributes"])
-        return f"""Sugest a description of 20 to 30 wordsfor a business that is a {business_type} related to  {keywords}. Return as a JSON string with description key.Example:{{"description":"the description"}}"""
+    
+        return f"""Create a description with 20 to 30 words, an acronym and a slogan for a {business_type} called {name} related to  {keywords}.They must be  {atributes}. Return as a JSON string. Ex:{{"description":"","slogan":"Get new gen with business generator","acronym":"BG"}}"""
     response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=_generate_brand_description_prompt(business_info),
+            prompt=_generate_brand_name_prompt(business_info),
             temperature=0.7,
             presence_penalty=1.5,
-            max_tokens=1000
-        )
-    return json.loads(response["choices"][0]["text"])
-
-def _generate_brand_slogan(business_info,name):
-    def _generate_brand_slogan_prompt(business_info):
-        def _separate_by_comas(list):
-            separeted_string=""
-            for word in list:
-                separeted_string+=word+","
-            return separeted_string
-
-        business_type=business_info["type"]
-        keywords = _separate_by_comas(business_info["keywords"])
-        atributes = _separate_by_comas(business_info["atributes"])
-        return f"""Sugest a slogan for a business named{name} that is a {business_type} related to  {keywords}.The slogan must be  {atributes}.Return as a JSON string with the slogan key.Example:{{"name":"name"}}"""
-    response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=_generate_brand_slogan_prompt(business_info),
-            temperature=0.7,
-            presence_penalty=1,
             max_tokens=400
         )
     return json.loads(response["choices"][0]["text"])
 
-def _generate_brand_colors(business_info):
-    def _generate_brand_colors_prompt(business_info):
-        def _separate_by_comas(list):
-            separeted_string=""
-            for word in list:
-                separeted_string+=word+","
-            return separeted_string
 
-        business_type=business_info["type"]
-        keywords = _separate_by_comas(business_info["keywords"])
-        atributes = _separate_by_comas(business_info["atributes"])
-        return f"""Sugest a color scheme of 4 colors for a business that is a {business_type} related to  {keywords}.The color scheme must be  {atributes}.Return as a JSON string with colors hex.Example:{{"colors":["#fffff","#000000"]}}."""
-    response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=_generate_brand_colors_prompt(business_info),
-            temperature=0.7,
-            max_tokens=400
-        )
-    return json.loads(response["choices"][0]["text"])
-
-def _generate_brand_font(business_info):
+def _generate_brand_font_and_colors(business_info):
     def _generate_brand_font_prompt(business_info):
         def _separate_by_comas(list):
             separeted_string=""
@@ -132,7 +96,7 @@ def _generate_brand_font(business_info):
 
         keywords = _separate_by_comas(business_info["keywords"])
         atributes = _separate_by_comas(business_info["atributes"])
-        return f"""Sugest a Google Font,except Roboto, for the logo of a business related to  {keywords}.The font must be  {atributes}.Return as a JSON string with the font name.Example:{{"font":"Arial"}}."""
+        return f"""Sugest a Google Font,except Roboto, and a color scheme of 4 colors for the logo of a business related to  {keywords}.The font must be  {atributes}.Return as a JSON string.Ex:{{"font":"Arial","colors":["#fffff","#000000"]}}."""
     response = openai.Completion.create(
             model="text-davinci-003",
             prompt=_generate_brand_font_prompt(business_info),
